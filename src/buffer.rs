f785987afd2e1270ba2dc20a3cf231c51e9a2ebf
ad-chaos <santoshrajpurohit89@gmail.com>
2026-03-23 12:33:17 +0530

    Separate moving around from rendering
    So after realising that I'll have to deal with unicode I think it makes
    a lot of sense to contain all the logic for moving around a cursor and
    scroll positions in `buffer::Buffer` and the rendering inside
    `pager::Pager`.

--- src/buffer.rs
+++ src/buffer.rs
@@ -3,6 +3,8 @@ use crate::{Pos, strip_ansi};
 pub struct Buffer {
     // Last rendered buffer position
     pub(crate) bpos: Pos,
+    // Current cursor position
+    pub(crate) cpos: Pos,
     // Style contents to be paged
     styled: String,
     // Line spans of paged contents
@@ -21,11 +23,16 @@ impl Buffer {
 
         Buffer {
             bpos: Pos::zero(),
+            cpos: Pos::zero(),
             styled,
             styled_lines,
             plain,
             plain_lines,
         }
+    }
+
+    pub fn lines(&self) -> usize {
+        self.styled_lines.len()
     }
 
     pub fn line_indicies(&self, start: usize, len: usize) -> impl Iterator<Item = (usize, &str)> {
@@ -35,23 +42,20 @@ impl Buffer {
             .enumerate()
     }
 
+    pub fn scroll_up(&mut self, scroll: usize) {
+        self.bpos.line = self.bpos.line.saturating_add(scroll);
+    }
+
+    pub fn scroll_down(&mut self, scroll: usize) {
+        self.bpos.line = self.bpos.line.saturating_sub(scroll);
+    }
+
+    pub fn scroll_to(&mut self, line: usize) {
+        self.bpos.line = line
+    }
+
     pub fn word_right_from(&self, line: usize, col: usize) -> usize {
-        let (lstart, lend) = self.plain_lines[line];
-        let bline = &self.plain[lstart..lend];
-
-        // Find the first non 'isk' character
-        let Some(nisk) =
-            bline[col..].find(|c| !matches!(c, 'A'..='Z' | 'a'..='z' | '_' | '0'..='9'))
-        else {
-            return col;
-        };
-
-        // // Find the next 'isk' character
-        let ncol = bline[col..][nisk..]
-            .find(|c| matches!(c, 'A'..='Z' | 'a'..='z' | '_' | '0'..='9'))
-            .unwrap_or(0);
-
-        col + nisk + ncol
+        todo!()
     }
 }
 
--- src/pager.rs
+++ src/pager.rs
@@ -14,12 +14,8 @@ use crate::{Buffer, Pos};
 pub struct Pager {
     // Contents to be paged
     buffer: Buffer,
-    // Number of lines within the buffer
-    lines: usize,
     // Terminal size (cols, rows)
     tsize: Pos,
-    // Cursor Position
-    cpos: Pos,
     // Stdout attached to this process
     stdout: Stdout,
     // Active VimMode
@@ -41,16 +37,13 @@ impl VimMode {
 
 impl Pager {
     pub fn new(buffer: String, stdout: Stdout) -> Pager {
-        let lines = buffer.lines().count();
         let (columns, rows) = size().expect("couldn't get terminal size");
         let tsize = Pos::new(columns as usize, rows as usize);
         let buffer = Buffer::new(buffer);
         Pager {
             buffer,
-            lines,
             stdout,
             tsize,
-            cpos: Pos::zero(),
             mode: VimMode::Normal,
         }
     }
@@ -82,7 +75,7 @@ impl Pager {
             }
 
             self.stdout
-                .queue(MoveTo(self.cpos.col as u16, self.cpos.line as u16))?
+                .queue(MoveTo(self.buffer.cpos.col as u16, self.buffer.cpos.line as u16))?
                 .queue(EndSynchronizedUpdate)?;
             self.stdout.flush()?;
         }
@@ -102,7 +95,7 @@ impl Pager {
                 .queue(Clear(ClearType::CurrentLine))?
                 .queue(Print(line))?;
         }
-        self.cpos = cursor::position().map(|(x, y)| Pos::new(x as usize, y as usize))?;
+        self.buffer.cpos = cursor::position().map(|(x, y)| Pos::new(x as usize, y as usize))?;
         Ok(())
     }
 
@@ -111,7 +104,7 @@ impl Pager {
 
         self.stdout.queue(ScrollUp(scroll as u16))?;
         self.paint_lines(buf_start, cur_start, scroll)?;
-        self.buffer.bpos.line = self.buffer.bpos.line.saturating_add(scroll);
+        self.buffer.scroll_up(scroll);
 
         Ok(())
     }
@@ -121,19 +114,19 @@ impl Pager {
 
         self.stdout.queue(ScrollDown(scroll as u16))?;
         self.paint_lines(buf_start, cur_start, scroll)?;
-        self.buffer.bpos.line = self.buffer.bpos.line.saturating_sub(scroll);
+        self.buffer.scroll_down(scroll);
 
         Ok(())
     }
 
     fn render_first(&mut self) -> io::Result<()> {
-        self.buffer.bpos.line = self.tsize.line;
+        self.buffer.scroll_to(self.tsize.line);
         self.paint_lines(0, 0, self.tsize.line)
     }
 
     fn render_last(&mut self) -> io::Result<()> {
-        self.buffer.bpos.line = self.lines;
-        self.paint_lines(self.lines - self.tsize.line, 0, self.tsize.line)
+        self.buffer.scroll_to(self.buffer.lines());
+        self.paint_lines(self.buffer.lines() - self.tsize.line, 0, self.tsize.line)
     }
 
     fn handle_normal(&mut self, key: KeyEvent) -> io::Result<()> {
@@ -141,28 +134,28 @@ impl Pager {
         self.mode = match key.code {
             KeyCode::Char('q') => Quit,
             KeyCode::Char('j') => {
-                if self.cpos.line + 1 < self.tsize.line {
-                    self.cpos = self.cpos.down();
-                } else if self.buffer.bpos.line < self.lines {
+                if self.buffer.cpos.line + 1 < self.tsize.line {
+                    self.buffer.cpos = self.buffer.cpos.down();
+                } else if self.buffer.bpos.line < self.buffer.lines() {
                     self.render_scroll_up(1)?;
                 }
                 Normal
             }
             KeyCode::Char('k') => {
-                if self.cpos.line != 0 {
-                    self.cpos = self.cpos.up();
+                if self.buffer.cpos.line != 0 {
+                    self.buffer.cpos = self.buffer.cpos.up();
                 } else if self.buffer.bpos.line > self.tsize.line {
                     self.render_scroll_down(1)?;
                 }
                 Normal
             }
             KeyCode::Char('h') => {
-                self.cpos = self.cpos.left();
+                self.buffer.cpos = self.buffer.cpos.left();
                 Normal
             }
             KeyCode::Char('l') => {
-                self.cpos = self.cpos.right();
-                self.cpos.col = self.cpos.col.min(self.tsize.col - 1);
+                self.buffer.cpos = self.buffer.cpos.right();
+                self.buffer.cpos.col = self.buffer.cpos.col.min(self.tsize.col - 1);
                 Normal
             }
             KeyCode::Char('G') => {
@@ -170,30 +163,30 @@ impl Pager {
                 Normal
             }
             KeyCode::Char('0') => {
-                self.cpos.col = 0;
+                self.buffer.cpos.col = 0;
                 Normal
             }
             KeyCode::Char('$') => {
-                self.cpos.col = self.tsize.col;
+                self.buffer.cpos.col = self.tsize.col;
                 Normal
             }
             KeyCode::Char('H') => {
-                self.cpos.line = 0;
+                self.buffer.cpos.line = 0;
                 Normal
             }
             KeyCode::Char('L') => {
-                self.cpos.line = self.tsize.line - 1;
+                self.buffer.cpos.line = self.tsize.line - 1;
                 Normal
             }
             KeyCode::Char('M') => {
-                self.cpos.line = (self.tsize.line - 1) / 2;
+                self.buffer.cpos.line = (self.tsize.line - 1) / 2;
                 Normal
             }
             KeyCode::Char('g') => WaitG,
             KeyCode::Char('w') => {
-                self.cpos.col = self.buffer.word_right_from(
-                    self.buffer.bpos.line - (self.tsize.line - self.cpos.line),
-                    self.cpos.col,
+                self.buffer.cpos.col = self.buffer.word_right_from(
+                    self.buffer.bpos.line - (self.tsize.line - self.buffer.cpos.line),
+                    self.buffer.cpos.col,
                 );
                 Normal
             }
@@ -209,7 +202,7 @@ impl Pager {
             VimMode::WaitG => {
                 if let KeyCode::Char('g') = key.code {
                     self.render_first()?;
-                    self.cpos = Pos::zero();
+                    self.buffer.cpos = Pos::zero();
                     self.mode = VimMode::Normal
                 }
             }
